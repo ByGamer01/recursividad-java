6dc01e1e180e8ca3a2c41ed2503fc4b65ecacf73
ByGamer01 <davidgilrosa1@gmail.com>
2026-01-29 13:09:16 +0100

    w

--- recursividad-java/Ejercicio3/src/App.java
+++ recursividad-java/Ejercicio3/src/App.java
@@ -3,8 +3,8 @@ public class App {
     public static boolean isIncreasing(int n) {
         if(n < 10) return true;
 
-        int ultimo = n % 10;
-        int penultimo = (n / 10) % 10;
+        int ultimo = n % 10; // Para extraer
+        int penultimo = (n / 10) % 10; // Para continuar
 
         if (penultimo > ultimo) return false;
         return isIncreasing(n / 10);
